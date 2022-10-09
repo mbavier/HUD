e5c1690341c046528b7766a7de1d0cd186ba84bd
Matthew Bavier <baviermatthew@gmail.com>
2022-10-08 23:20:56 -0700

    a

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
-let map, infoWindow;
+let map, locationMarker;
 
+
+function success(position) {
+  const pos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude,
+  };
+  map.setCenter(pos);
+  locationMarker.setPosition(pos);
+}
 
 function getLocation() {
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.open(map);
-        map.setCenter(pos);
-      },
+    navigator.geolocation.watchPosition(
+      success,
       () => {
-        handleLocationError(true, infoWindow, map.getCenter());
+        handleLocationError(true, map.getCenter());
       }
     );
   } else {
     // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+    handleLocationError(false, map.getCenter());
   }
 }
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
+    zoom: 15,
   });
-  infoWindow = new google.maps.InfoWindow();
+
+  const image =
+    "https://mbavier.github.io/HUD/location.png";
+
+  locationMarker = new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644 },
+    map,
+    icon: image
+  });
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+function handleLocationError(browserHasGeolocation, pos) {
+  console.log("no location");
 }
 
+let time0, time1;
 
-
-function step() {
-  getLocation();
-  requestAnimationFrame(step);
-}
-
+// function step() {
+//   time1 = performance.now();
+//   if (time1 - time0 >= 10000) {
+//     ;
+//     time0 = time1;
+//   }
+//   requestAnimationFrame(step);
+  
+// }
 document.getElementById('start').onclick = () => {
+  time0 = performance.now();
   document.getElementById('start').style.display = "none";
-  window.initMap = initMap;
-  requestAnimationFrame(step);
+  document.getElementById('map').style.visibility = "visible";
+  getLocation()
+  //requestAnimationFrame(step);
 };
 
+window.initMap = initMap;
