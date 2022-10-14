221992aef219627ff7389706fa6a3b247e3c593d
Matthew Bavier <baviermatthew@gmail.com>
2022-10-14 12:26:48 -0700

    stay landscape

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let xDisplacement = document.getElementById('x-displacement');
 let yDisplacement = document.getElementById('y-displacement');
 
 if ('serviceWorker' in navigator) {
+  screen.orientation.lock('landscape');
   window.addEventListener('load', function() {
     this.navigator.serviceWorker.register('service-worker.js').then(function(registration) {
       console.log('Registered');
